2b1b974c4606465cbc2cefb97507433f37b51a41
hsh519 <suk000519@naver.com>
2024-02-21 12:25:45 +0900

    Exercise 6-4 Edit Record

--- force-app/Exercises/main/default/lwc/tripReportBrowser/tripReportBrowser.js
+++ force-app/Exercises/main/default/lwc/tripReportBrowser/tripReportBrowser.js
@@ -65,4 +65,12 @@ export default class TripReportBrowser extends LightningElement {
 	onBtnNewClick() {
 		this.changeTripReportMode("add");
 	}
+
+	handleRowClick(event) {
+		this.selectedRecordId = event.detail.pk;
+	}
+
+	handleRowDblClick() {
+		this.changeTripReportMode("edit");
+	}
 }
